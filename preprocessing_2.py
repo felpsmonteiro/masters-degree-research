a870d16af130954af546edcbb7d8c7e90ff35c0b
Felipe Timbo <timbo.felipe@gmail.com>
2023-03-15 20:53:12 -0300

    remocao de metodos desnecessarios e refactoring

--- preprocessing_2.py
+++ preprocessing_2.py
@@ -15,17 +15,30 @@ class PreProcessing():
 
     def preprocess_dataset(self):
         traf_df = pd.read_csv(self.dataset_url, sep=';', nrows=None)
-        df_counts = traf_df.sort_values(['DESTPORT'],ascending=False).groupby(['DESTPORT', 'PROTOCOL', 'SERVICE'])['SERVICE'].count().to_csv('counts.csv', sep=';')
-        df_counts = pd.read_csv('counts.csv', sep=';', nrows = None)
-        df_counts = df_counts.rename(columns={'SERVICE.1':'COUNT'})
+        df_counts = traf_df.sort_values(['DESTPORT'],ascending=False).groupby(['DESTPORT', 'PROTOCOL', 'SERVICE']).size().reset_index(name='COUNT') # .to_csv('counts.csv', sep=';')
+        # df_counts = pd.read_csv('counts.csv', sep=';', nrows = None)
+        # df_counts = df_counts.rename(columns={'SERVICE.1':'COUNT'})
 
-        df_counts_array = df_counts.to_numpy()
+        # df_counts_array = df_counts.to_numpy()
 
-        df_prot, df_serv, df_port = df_counts.groupby(['PROTOCOL'])['COUNT'].sum().reset_index(), df_counts.groupby(['SERVICE'])['COUNT'].sum().reset_index(), df_counts.groupby(['DESTPORT'])['COUNT'].sum().reset_index()
+        # df_prot, df_serv, df_port = df_counts.groupby(['PROTOCOL'])['COUNT'].sum().reset_index(), df_counts.groupby(['SERVICE'])['COUNT'].sum().reset_index(), df_counts.groupby(['DESTPORT'])['COUNT'].sum().reset_index()
 
-        protocol, service, ports = df_prot['PROTOCOL'].unique(), df_serv['SERVICE'].unique(), df_port['DESTPORT'].unique()
-        prot_count, serv_count, ports_count = fc.Count(protocol, traf_df, "PROTOCOL"), fc.Count(service, traf_df, "SERVICE"), fc.Count(ports, traf_df, "DESTPORT")
+        df_prot = traf_df.groupby(['PROTOCOL']).size().reset_index(name='COUNT')
+        df_serv = traf_df.groupby(['SERVICE']).size().reset_index(name='COUNT')
+        df_port = traf_df.groupby(['DESTPORT']).size().reset_index(name='COUNT')
+
+        # protocol, service, ports = df_prot['PROTOCOL'].unique(), df_serv['SERVICE'].unique(), df_port['DESTPORT'].unique()
         
+        protocol = df_prot['PROTOCOL'].to_numpy()
+        service = df_serv['SERVICE'].to_numpy()
+        ports = df_port['DESTPORT'].to_numpy()
+
+        # prot_count, serv_count, ports_count = fc.Count(protocol, traf_df, "PROTOCOL"), fc.Count(service, traf_df, "SERVICE"), fc.Count(ports, traf_df, "DESTPORT")
+        
+        prot_count = df_prot['COUNT'].to_numpy()
+        serv_count = df_serv['COUNT'].to_numpy()
+        ports_count = df_port['COUNT'].to_numpy()
+
         new_data = {
                         'protocols' : prot_count,
                         'services' : serv_count,
@@ -39,7 +52,7 @@ class PreProcessing():
             pkl.dump(new_data, f)
         
         with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', self.dataset_name + '_tab_2.pkl')), 'wb') as f:
-            pkl.dump(df_counts_array, f)
+            pkl.dump(df_counts, f)
 
 
 if __name__ == "__main__":
--- proposed_approach_2.py
+++ proposed_approach_2.py
@@ -22,7 +22,7 @@ class ProposedApproach():
             
             #  0. Ler dados tabelão salvo pre processamento
             with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', dataset + '_tab_2.pkl')), 'rb') as f:
-                data2 = pkl.load(f)
+                data = pkl.load(f)
          
             for e in self.es:
                 print('--------- eps ' + str(e) + ' ---------')
@@ -31,17 +31,17 @@ class ProposedApproach():
                     print('...... run ' + str(r) + ' ......')
                     
                     # 1. Add ruido no tabelão -- Nova coluna eps[e];
-                    data2 = pd.DataFrame(data2, columns = ['DESTPORT','PROTOCOL','SERVICE', 'COUNT'])
-                    data2[e] = mechanisms.geometric(data2['COUNT'].to_numpy(), e)
+                    # data = pd.DataFrame(data, columns = ['DESTPORT','PROTOCOL','SERVICE', 'COUNT'])
+                    data[e] = mechanisms.geometric(data['COUNT'].to_numpy(), e)
                                         
                     # 2. services_noisy = data2.groupby('SERVICE')[e].sum().to_numpy;
-                    services_noisy = data2.groupby('SERVICE')[e].sum()
+                    services_noisy = data.groupby('SERVICE')[e].sum()
                     
                     # 3. protocols_noisy = data.groupby('PROTOCOL')[e].sum().to_numpy();
-                    protocols_noisy = data2.groupby('PROTOCOL')[e].sum()
+                    protocols_noisy = data.groupby('PROTOCOL')[e].sum()
                     
                     # 4. ports_noisy = data.groupby('DESTPORT')[e].sum().to_numpy;
-                    ports_noisy = data2.groupby('DESTPORT')[e].sum()
+                    ports_noisy = data.groupby('DESTPORT')[e].sum()
                    
                     protocols_post_processed = mechanisms.post_processing(protocols_noisy.to_numpy())
                     services_post_processed = mechanisms.post_processing(services_noisy.to_numpy())
@@ -59,13 +59,13 @@ class ProposedApproach():
 if __name__ == "__main__":
 
     datasets = [
-                'local',
-                'kaggle'    
+                'kaggle',
+                'local'    
                 ]
 
     es = [ .01, .1, .5, 1 ] 
 
-    runs = 10
+    runs = 50
 
     approach = ProposedApproach(datasets, es, runs)
     approach.run()
