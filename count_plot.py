7cae1977b9b1ced8bece47b7a5d4ae44775b2c75
Felipe Monteiro <monteiro.flp@gmail.com>
2023-03-18 11:06:15 -0300

    New Service Stream Images

--- count_plot.py
+++ count_plot.py
@@ -7,22 +7,27 @@ import matplotlib.patches as mpatches
 import seaborn as sns
 import functions as fc
 
+
+ft = 16
+
 def addlabels(x,y):
     for i in range(len(x)):
-        plt.text(i, y[i], y[i], ha = 'center')
+        plt.text(i, y[i], y[i], ha = 'center', fontsize=ft)
 
 y = [60, 30, 9, 1] 
 x = ['Netflix', 'AmazonVideo', 'AppleTv', 'Looke']
 
-fig, ax = plt.subplots(figsize =(9, 5))
-ax.grid(b = True, color ='grey',
+fig, ax = plt.subplots(figsize =(10, 5))
+ax.grid(visible = True, color ='grey',
         linestyle ='-.', linewidth = 0.5,
         alpha = 0.2)
 
-plt.bar(x, y)
-plt.xticks(x)
+plt.bar(x, y, color=('#1f306e', '#553772', '#8f3b76','#c7417b'))
+#plt.bar(x, y)
+plt.xticks(x, fontsize=ft-2)
+plt.yticks(fontsize=ft-2)
 addlabels(x, y)
-plt.xlabel('Serviços de Streaming')
-plt.ylabel('Acessos')
+plt.xlabel('Serviços de Streaming', fontsize=ft+2)
+plt.ylabel('Acessos', fontsize=ft+2)
 plt.tight_layout()
 plt.show()
