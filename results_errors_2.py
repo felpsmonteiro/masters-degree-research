7aa5810c95d84a4906d5635f1c5422c0d6439039
felpsmonteiro <monteiro.flp@gmail.com>
2023-04-02 18:49:58 -0300

    Pipeline Working Properly After DataFrame Added

--- results_errors_2.py
+++ results_errors_2.py
@@ -8,6 +8,8 @@ import mechanisms
 import err_metrics
 import graphics
 
+pd.set_option('display.max_rows', 1000)
+
 class Results():
 
     def __init__(self, 
@@ -29,13 +31,13 @@ class Results():
             with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', dataset + '_2.pkl')), 'rb') as f:
 	            data = pkl.load(f)
             
+            df_main = pd.DataFrame()
+            
             errors_1 = {}  # approach errors
             errors_2 = {}  # geometric errors
             errors_3 = {}  # log-laplace errors
             errors_4 = {}  # privbayes errors
-            
-            df_main = pd.DataFrame()
-            
+
             for count in self.counts:
                 errors_1[count] = {}
                 errors_2[count] = {}
@@ -97,22 +99,9 @@ class Results():
                             error_4 = err_metrics.calculate(error_metr, data[count], data4[count])
                             errors_list_4[count][error_metr].append(error_4) 
                 
-                # for error_metr in self.error_metrics:
-                #     for count in self.counts:
-                #         ego_metric_mean_1 = np.mean(errors_list_1[count][error_metr])
-                #         errors_1[count][error_metr].append(float("{:.2f}".format(ego_metric_mean_1)))
-
-                #         ego_metric_mean_2 = np.mean(errors_list_2[count][error_metr])
-                #         errors_2[count][error_metr].append(float("{:.2f}".format(ego_metric_mean_2)))
-
-                #         ego_metric_mean_3 = np.mean(errors_list_3[count][error_metr])
-                #         errors_3[count][error_metr].append(float("{:.2f}".format(ego_metric_mean_3)))
-
-                #         ego_metric_mean_4 = np.mean(errors_list_4[count][error_metr])
-                #         errors_4[count][error_metr].append(float("{:.2f}".format(ego_metric_mean_4)))
                 
-                df = pd.DataFrame(columns=["Legends", "Epslon"])
-                      
+                df = pd.DataFrame(columns=["Legends", "Epsilon"])
+                
                 for error_metr in self.error_metrics:
                     for count in self.counts:
                         
@@ -120,74 +109,37 @@ class Results():
                         
                         if df.empty:
                                 ego_metric_mean_1 = errors_list_1[count][error_metr]
-                                new_rows1 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_1, 'Legends': "Abordagem Proposta", 'Epslon': e })
+                                new_rows1 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_1, 'Legends': "Abordagem Proposta", 'Epsilon': e })
                                 df = df.append(new_rows1, ignore_index=True)
                                 
                                 ego_metric_mean_2 = errors_list_2[count][error_metr]
-                                new_rows2 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_2, 'Legends': "Mecanismo Geométrico", 'Epslon': e  })
+                                new_rows2 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_2, 'Legends': "Mecanismo Geométrico", 'Epsilon': e  })
                                 df = df.append(new_rows2, ignore_index=True)
         
                                 ego_metric_mean_3 = errors_list_3[count][error_metr]
-                                new_rows3 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_3, 'Legends': "Mecanismo Log-Laplace", 'Epslon': e  })
+                                new_rows3 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_3, 'Legends': "Mecanismo Log-Laplace", 'Epsilon': e  })
                                 df = df.append(new_rows3, ignore_index=True)
                                                         
                                 ego_metric_mean_4 = errors_list_4[count][error_metr]
-                                new_rows4 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_4, 'Legends': "Privbayes", 'Epslon': e  })
+                                new_rows4 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_4, 'Legends': "Privbayes", 'Epsilon': e  })
                                 df = df.append(new_rows4, ignore_index=True)
 
                         else:
-                            for i in range(len(es):
-                                if e > es[0]:
-                                    ego_metric_mean_1 = errors_list_1[count][error_metr]
-                                    new_rows1 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_1, 'Legends': "Abordagem Proposta", 'Epslon': e })
-                                    df = df.append(new_rows1, ignore_index=True)
-                                    
-                                    ego_metric_mean_2 = errors_list_2[count][error_metr]
-                                    new_rows2 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_2, 'Legends': "Mecanismo Geométrico", 'Epslon': e  })
-                                    df = df.append(new_rows2, ignore_index=True)
+                                ego_metric_mean_1 = errors_list_1[count][error_metr]
+                                df.iloc[0:10, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_1
+
+                                ego_metric_mean_2 = errors_list_2[count][error_metr]
+                                df.iloc[10:20, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_2
+
+                                ego_metric_mean_3 = errors_list_3[count][error_metr]
+                                df.iloc[20:30, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_3
+
+                                ego_metric_mean_4 = errors_list_4[count][error_metr]
+                                df.iloc[30:40, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_4
+
+                df_main = df_main.append(df)
             
-                                    ego_metric_mean_3 = errors_list_3[count][error_metr]
-                                    new_rows3 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_3, 'Legends': "Mecanismo Log-Laplace", 'Epslon': e  })
-                                    df = df.append(new_rows3, ignore_index=True)
-                                                            
-                                    ego_metric_mean_4 = errors_list_4[count][error_metr]
-                                    new_rows4 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_4, 'Legends': "Privbayes", 'Epslon': e  })
-                                    df = df.append(new_rows4, ignore_index=True)
-                                
-                                else:
-                                
-                                    ego_metric_mean_1 = errors_list_1[count][error_metr]
-                                    df.iloc[0:10, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_1
-                                        
-                                    ego_metric_mean_2 = errors_list_2[count][error_metr]
-                                    df.iloc[10:20, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_2
-                                    
-                                    ego_metric_mean_3 = errors_list_3[count][error_metr]
-                                    df.iloc[20:30, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_3
-                                    
-                                    ego_metric_mean_4 = errors_list_4[count][error_metr]
-                                    df.iloc[30:40, df.columns.get_loc(f"{count}_{error_metr}")] = ego_metric_mean_4
-
-                df_main = pd.concat([df_main, df], ignore_index=True) 
-                           
-                            # else:
-                            
-                            #     ego_metric_mean_1 = errors_list_1[count][error_metr]
-                            #     new_rows1 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_1, 'Legends': "Abordagem Proposta", 'Epslon': e })
-                            #     df = df.append(new_rows1, ignore_index=True)
-                                
-                            #     ego_metric_mean_2 = errors_list_2[count][error_metr]
-                            #     new_rows2 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_2, 'Legends': "Mecanismo Geométrico", 'Epslon': e  })
-                            #     df = df.append(new_rows2, ignore_index=True)                            
-                                
-                            #     ego_metric_mean_3 = errors_list_3[count][error_metr]
-                            #     new_rows3 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_3, 'Legends': "Mecanismo Log-Laplace", 'Epslon': e  })
-                            #     df = df.append(new_rows3, ignore_index=True)
-                                
-                            #     ego_metric_mean_4 = errors_list_4[count][error_metr]
-                            #     new_rows4 = pd.DataFrame({f"{count}_{error_metr}": ego_metric_mean_4, 'Legends': "Privbayes", 'Epslon': e  })
-                            #     df = df.append(new_rows4, ignore_index=True)                      
-
+            df_main = df_main.reset_index()
                         
             legends = [
                         'Abordagem Proposta',
@@ -196,27 +148,28 @@ class Results():
                         'Privbayes'
                     ]
 
+            
+            
+            
             for error_metr in self.error_metrics:
                 for count in self.counts:
-                    y = []
-                    y.append(errors_1[count][error_metr])
-                    y.append(errors_2[count][error_metr])
-                    y.append(errors_3[count][error_metr])
-                    y.append(errors_4[count][error_metr])
                     
-                    print(y)
+                    sns.lineplot(x='Epsilon', y='y', data=df, ci=95)
+            #         y = []
+            #         y.append(errors_1[count][error_metr])
+            #         y.append(errors_2[count][error_metr])
+            #         y.append(errors_3[count][error_metr])
+            #         y.append(errors_4[count][error_metr])
                     
-                    # data = {'y': y, 'x': legends}
-                    # df = pd.DataFrame(data)
-                    # print(df)
-                    # df = pd.DataFrame(my_array, columns = ['Column_A','Column_B','Column_C'])
-                    # print(dataset, error_metr , count), print(y)
- 
-                    path_result = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'results', dataset, error_metr, '%s_%s_%s_result_log_2.png' % ( dataset, count, error_metr) ))  
-                    graphics.line_plot(np.array(self.es), np.array(y), xlabel='$\epsilon$', ylabel= error_metr, ylog=True, line_legends=legends, figsize=(5, 5), path=path_result)
+            #         print(y)
+                    
 
-                    path_result2 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'results', dataset,  error_metr, '%s_%s_%s_result_2.png' % ( dataset, count, error_metr) ))  
-                    graphics.line_plot(np.array(self.es), np.array(y), xlabel='$\epsilon$', ylabel= error_metr, ylog=False, line_legends=legends, figsize=(5, 5), path=path_result2)
+ 
+            #         path_result = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'results', dataset, error_metr, '%s_%s_%s_result_log_2.png' % ( dataset, count, error_metr) ))  
+            #         graphics.line_plot(np.array(self.es), np.array(y), xlabel='$\epsilon$', ylabel= error_metr, ylog=True, line_legends=legends, figsize=(5, 5), path=path_result)
+
+            #         path_result2 = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'results', dataset,  error_metr, '%s_%s_%s_result_2.png' % ( dataset, count, error_metr) ))  
+            #         graphics.line_plot(np.array(self.es), np.array(y), xlabel='$\epsilon$', ylabel= error_metr, ylog=False, line_legends=legends, figsize=(5, 5), path=path_result2)
 
 if __name__ == "__main__":
 
