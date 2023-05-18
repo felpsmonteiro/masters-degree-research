3a817b2c553c285807c94db4e31f636f2e3287d1
Felipe Monteiro <monteiro.flp@gmail.com>
2023-05-18 15:36:21 -0300

    Updating Elapsed Times

--- preprocessing_2.py
+++ preprocessing_2.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
-import functions as fc
 import pickle as pkl
+import time
 
 class PreProcessing():
 
@@ -14,6 +14,9 @@ class PreProcessing():
         self.dataset_name = dataset_name 
 
     def preprocess_dataset(self):
+        
+        starttime = time.time()
+        
         traf_df = pd.read_csv(self.dataset_url, sep=';', nrows=None)
         
         df_counts = traf_df.groupby(['DESTPORT', 'PROTOCOL', 'SERVICE']).size().reset_index(name='COUNT')
@@ -45,16 +48,32 @@ class PreProcessing():
         
         with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', self.dataset_name + '_tab_2.pkl')), 'wb') as f:
             pkl.dump(df_counts, f)
+        
+        endtime = time.time()
+        elapsed_time = endtime - starttime
 
+        print('Elapsed Time:', elapsed_time, 'seconds\n')
+
+        exectime = {
+                        'starttime' : starttime,
+                        'endtime' : endtime,
+                        'time' : elapsed_time
+                    }
+
+        with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', self.dataset_name, '%s_%s_%s_executiontime_preprocessing_2.pkl' % ( self.dataset_name ))), 'wb') as f:
+                        pkl.dump(exectime, f)        
+        
 
 if __name__ == "__main__":
 
     url_kaggle = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', 'Unicauca-dataset-April-June-2019-Network-flows.csv' ))
     url_kagglel = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', 'Unicauca-dataset-April-June-2020-Network-flows_2.csv' ))
     url_local = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', 'traffic_table4.csv' ))
+    
     pre_proc_kaggle = PreProcessing(url_kaggle, 'kaggle')
     pre_proc_kagglel = PreProcessing(url_kaggle, 'kagglel')
     pre_proc_local = PreProcessing(url_local, 'local' )
+    
     pre_proc_kaggle.preprocess_dataset()
     pre_proc_kagglel.preprocess_dataset()
     pre_proc_local.preprocess_dataset()
