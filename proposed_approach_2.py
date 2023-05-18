e53b97bcdb85b9d79f62473f00a3081a376eeee6
Felipe Monteiro <monteiro.flp@gmail.com>
2023-05-18 15:29:23 -0300

    Adding Elapsed Time into proposed_approach_2.py

--- proposed_approach_2.py
+++ proposed_approach_2.py
@@ -4,6 +4,7 @@ import numpy as np
 import functions as fc
 import pickle as pkl
 import mechanisms
+import time
 
 class ProposedApproach():
 
@@ -28,6 +29,8 @@ class ProposedApproach():
                 print('--------- eps ' + str(e) + ' ---------')
 
                 for r in range(self.runs):
+                    starttime = time.time()
+                    
                     print('...... run ' + str(r) + ' ......')
                     
                     # 1. Add ruido no tabelão -- Nova coluna eps[e];
@@ -56,17 +59,32 @@ class ProposedApproach():
                     
                     with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_approach_2.pkl' % ( dataset, e, r ))), 'wb') as f:
 	                    pkl.dump(noisy_data, f)
+                    
+                    endtime = time.time()
+                    elapsed_time = endtime - starttime
+        
+                    print('Elapsed Time:', elapsed_time, 'seconds\n')
 
+                    exectime = {
+                                    'starttime' : starttime,
+                                    'endtime' : endtime,
+                                    'time' : elapsed_time
+                                }
+        
+                    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_executiontime_approach_2.pkl' % ( dataset, e, r ))), 'wb') as f:
+                                    pkl.dump(exectime, f)
+                                    
 if __name__ == "__main__":
 
     datasets = [
                 'kaggle',
+                'kagglel',
                 'local'    
                 ]
 
     es = [ .1, .5, 1 ] 
 
-    runs = 10
+    runs = 50
 
     approach = ProposedApproach(datasets, es, runs)
     approach.run()
