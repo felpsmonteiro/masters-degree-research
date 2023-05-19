02c5c42ff6323fe7e1e78daa14560b153bbcd468
Felipe Monteiro <monteiro.flp@gmail.com>
2023-05-19 09:56:13 -0300

    Running Baseline Log-Laplace 50 Times and Renaming Files

--- baseline2-log-laplace.py
+++ baseline2-log-laplace.py
@@ -19,7 +19,7 @@ class LogLaplace():
     def run(self):
         for dataset in self.datasets:
             print('***************** DATASET ' + dataset + ' *****************')
-            with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', dataset + '_2.pkl')), 'rb') as f:
+            with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'Datasets', dataset + '.pkl')), 'rb') as f:
 	            data = pkl.load(f)
 
             # protocols, services, ports = data[0], data[1], data[2]
@@ -42,7 +42,7 @@ class LogLaplace():
                         'ports' : ports_noisy
                     }
 
-                    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_log_laplace_2.pkl' % ( dataset, e, r ))), 'wb') as f:
+                    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_log_laplace.pkl' % ( dataset, e, r ))), 'wb') as f:
 	                    pkl.dump(noisy_data, f)
 
                     endtime = time.time()
@@ -57,7 +57,7 @@ class LogLaplace():
                                     'time' : elapsed_time
                                 }
         
-                    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_executiontime_log_laplace_2.pkl' % ( dataset, e, r ))), 'wb') as f:
+                    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'exp', dataset, '%s_%s_%s_executiontime_log_laplace.pkl' % ( dataset, e, r ))), 'wb') as f:
                                     pkl.dump(exectime, f)
 
 if __name__ == "__main__":
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 
     es = [ .1, .5, 1 ] 
 
-    runs = 3
+    runs = 50
 
     approach = LogLaplace(datasets, es, runs)
     approach.run()
