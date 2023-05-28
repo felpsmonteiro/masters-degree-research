e80652e9288eced56a63a6c964d5c56ffeddcc26
Felipe Monteiro <monteiro.flp@gmail.com>
2023-05-28 15:01:21 -0300

    Updating Graphics.py and New .pngs

--- graphics.py
+++ graphics.py
@@ -61,18 +61,70 @@ def line_plot(x, ys, path=None, line_legends=None, legend_path=None,
     plt.clf()
     plt.close()
 
-def plot_line_graph(data=None, x=None, y=None, hue=None, style=None,
-                    title=None, xlabel=None, ylabel=None, filename=None,
-                    yscale='linear', save_path='.', dpi=300, figsize=(5,5)):
-    
-    sns.set_theme(style="darkgrid")
-    graph = sns.lineplot(data=df, x=x_col, y=y_col, hue=legend_col, style=legend_col, err_style='band', markers=True, dashes=False)
-    graph.set_title(title)
-    graph.set_xlabel(xlabel)
-    graph.set_ylabel(ylabel)
-    graph.set_yscale(yscale)
+def plot_line_graph(d, x, y, xticksize=None, yticksize=None, line_legends=None, path=None, estimator=None,
+                   xlabel=None, xlabelfontsize=None, ylabel=None, ylabelfontsize=None, legends_fontsize=None,
+                   title=None, xlog=False, ylog=False, themestyle=None, figsize=None, error=None,
+                   colors = None, figwidth=None, figheight=None, place=None, bottommargin=None):
+
+    if colors:
+        sns.set_theme(style=themestyle)
+        color = colors
+        sns.set_palette(sns.color_palette(color))
+        graph = sns.lineplot(data=d, x=x, y=y,
+                        hue=line_legends, style=line_legends, err_style=error,
+                        markers=True, dashes=False)
+        
+    else:
+        sns.set_theme(style=themestyle)
+        graph = sns.lineplot(data=d, x=x, y=y,
+                             hue=line_legends, style=line_legends, err_style=error,
+                             markers=True, dashes=False)
+        
     fig = graph.get_figure()
-    fig.savefig(f"{save_path}/{filename}")
+    
+    graph.legend(fontsize=legends_fontsize)
+    
+    if ylog:
+        graph.set_yscale('log')
+    # if xlog:
+        # plt.xscale('log')    
+    
+    if ylabel:
+        graph.set_ylabel(ylabel, fontsize=ylabelfontsize)
+    if xlabel:
+        graph.set_xlabel(xlabel, fontsize=xlabelfontsize)    
+    if title:
+        graph.set_title(title)
+                
+    if xticksize:
+        plt.xticks(fontsize=xticksize)
+    if yticksize:
+        plt.yticks(fontsize=yticksize)
+    
+    if figwidth:
+        fig.set_figwidth(figwidth)
+    if figheight:
+        fig.set_figheight(figheight)
+    if bottommargin:
+        plt.subplots_adjust(bottom=bottommargin)
+
+    if place:
+        plt.legend(loc=place)
+    if path:
+        if os.path.exists(path) == True:
+            graph.figure.savefig(path)
+            print('Graphic saved at: ' + path)
+        
+        else:
+            dir_path = os.path.dirname(os.path.realpath(path))
+            os.makedirs(dir_path, exist_ok=True)
+            graph.figure.savefig(path)
+            print('Graphic saved at: ' + path)
+    else:
+        plt.show()
+        
+    plt.clf()
+    plt.close()
       
 def plot_bar_graph(d, x, y, xticksize=None, yticksize=None, line_legends=None, path=None, estimator=None,
                    xlabel=None, xlabelfontsize=None, ylabel=None, ylabelfontsize=None, legends_fontsize=None,
