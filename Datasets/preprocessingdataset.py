23048c99dee4a420e3785f486e3ea22ee7fa639b
Felipe Monteiro <monteiro.flp@gmail.com>
2023-05-29 13:38:10 -0300

    Deleting old Dataset_UNSW and updating preprocessing.py

--- Datasets/preprocessingdataset.py
+++ Datasets/preprocessingdataset.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('Datasets/Darknet.CSV', sep=',')
 
-df = df.rename(columns={'dst_port': 'DESTPORT', 'proto': 'PROTOCOL', 'web_service': 'SERVICE'})
+df = pd.read_csv('Datasets/Copy of Darknet.csv', sep=';')
+df['SERVICE'] = df['Label'].astype(str) + '_' + df['Label.1'].astype(str)
 
-di = {1: 'ICMP', 
+df = df.rename(columns={'Dst Port': 'DESTPORT', 'Protocol': 'PROTOCOL'})
+# df = df.rename(columns={'dst_port': 'DESTPORT', 'proto': 'PROTOCOL', 'web_service': 'SERVICE'})
+
+di = {0: 'HOPOPT', 
       6: 'TCP',
       17: 'UDP'
       }
 
 df['PROTOCOL'] = df['PROTOCOL'].map(di)
 
-df = df[df['PROTOCOL'] != 'ICMP']
+df = df[df['PROTOCOL'] != 'HOPOPT']
 
-df = df.to_csv('Datasets/Unicauca-dataset-April-June-2020-Network-flows_2.csv', sep=';', index=False)
+df = df.to_csv('Datasets/Canadian-Institute-for-Cybersecurity.csv', sep=';', index=False)
